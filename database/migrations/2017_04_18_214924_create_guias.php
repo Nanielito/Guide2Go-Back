10fe9b5b68558991a8016d8df33296ae7aabfdd0
paharo <potopelao2@gmail.com>
2017-04-18 17:57:59 -0400

    Creacion de migracion de idiomas y de guia

---
+++ database/migrations/2017_04_18_214924_create_guias.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuias extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idiomas_id')->unsigned();
+            $table->float('costo');
+            $table->timestamps();
+
+            $table->foreign('idiomas_id')->references('id')->on('idiomas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guias');
+    }
+}
