41b54dccde9acb8cd3f6076f786fd07a2d49a858
paharo <potopelao2@gmail.com>
2017-04-18 18:10:53 -0400

    Creado migracion sub-zonas y paradas

---
+++ database/migrations/2017_04_18_215846_create_sub-zonas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubZonas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub-zonas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->polygon('poligono');
+            $table->string('nombre')->nullable();
+            $table->integer('zonas_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('zonas_id')->references('id')->on('zonas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub-zonas');
+    }
+}
---
+++ database/migrations/2017_04_18_220541_create_paradas.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParadas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paradas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('descripcion');
+            $table->string('nombre');
+            $table->string('foto');
+            $table->string('audio');
+            $table->integer('sub-zonas_id')->unsigned();
+            $table->point('punto');
+
+            $table->foreign('sub-zonas_id')->references('id')->on('sub-zonas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paradas');
+    }
+}
