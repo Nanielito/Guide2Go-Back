0de96ca31ed36908bdd5bab056358f4c6f786ea9
paharo <potopelao2@gmail.com>
2017-04-19 03:12:32 -0400

    Añadido recurso User con algunos metodos, falta añadir token

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('user', 'UserController', ['only' => ['show', 'store' , 'update', 'destroy', 'index' ], 
+	'parameters' => ['user' => 'id']]);
