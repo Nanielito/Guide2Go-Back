efac9b141e5ce61d8f54ce5c948e30837af48dfe
Oswald <oswald.capriles46@gmail.com>
2017-05-06 02:58:25 -0400

    Agregado ruta para la creacion de paradas

--- app/Parada.php
+++ app/Parada.php
@@ -12,5 +12,24 @@ class Parada extends GeoModel
 
     public function subZona() {
     	return $this->belongsTo('App\Sub_zona', 'sub_zonas_id');
-    }
+	}
+	
+	/**
+	 * Crear una parada en la base de datos
+	 *
+	 * @param  array 
+	 * @return Parada
+	 */
+	public static function store($args) {
+		
+		$stop = new Parada;
+		$stop->sub_zonas_id = $args['subzone'];
+		$stop->categoria_id = $args['category'];
+		$stop->nombre = $args['name'];
+		$stop->descripcion = $args['description'];
+		$stop->punto = $args['point'];
+		$stop->save();
+
+		return $stop;
+	}
 }
