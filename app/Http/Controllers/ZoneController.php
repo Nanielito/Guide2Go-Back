6ec6547a1788a49b339ff28a9af887784dac33e3
Andres <alabbe@uc.edu.com>
2017-06-07 15:02:33 -0500

    bn

--- app/Http/Controllers/ZoneController.php
+++ app/Http/Controllers/ZoneController.php
@@ -18,10 +18,10 @@ class ZoneController extends Controller
     public function index()
 	{
 		if (!JWTHelper::authenticate()) {
-			$response = ['error' => JWTHelper::authenticate() ];
+			$response = ['error' => 'error' ];
 			return \Response::json($response, 403);
 		}
-        return Zona::all();
+       return Zona::all();
     }
 
     /**
